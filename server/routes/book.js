2c9dc697def213c646517f7e47f6f5af29678ae0
cerdemrc <cerdemrc@gmail.com>
2021-10-04 14:42:23 +0300

    fix: wrapper middleware to routes

--- server/routes/book.js
+++ server/routes/book.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { getAllBooks, addBook, deleteBook } = require('../controllers/book')
+const { getAccessToRoute } = require('../middlewares/authorization/auth')
 const router = express.Router();
 
-router.get("/", getAllBooks)
-router.post("/", addBook)
-router.delete("/:id", deleteBook)
+router.get("/", getAccessToRoute, getAllBooks)
+router.post("/", getAccessToRoute, addBook)
+router.delete("/:id", getAccessToRoute, deleteBook)
 
 module.exports = router;
