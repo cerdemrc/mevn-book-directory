bed6612c8fcbe3d26b92dd86bbd678a06e3b596a
cerdemrc <cerdemrc@gmail.com>
2021-10-06 22:05:25 +0300

    fix: import path

--- client/src/views/Homepage.vue
+++ client/src/views/Homepage.vue
@@ -7,9 +7,9 @@
 </template>
 
 <script>
-import BookList from "./components/BookList.vue";
-import Header from "./components/Header.vue";
-import Welcome from "./components/Welcome.vue";
+import BookList from "../components/BookList.vue";
+import Header from "../components/Header.vue";
+import Welcome from "../components/Welcome.vue";
 export default {
   components: {
     BookList,
